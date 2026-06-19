ff468da4efc5becd6ae15b446d4ea179322fb149
Moses Agunbiade <agunbiademoses2009@gmail.com>
2026-06-19 18:34:36 +0100

    Added paths

--- frontend/src/components/navbar/Menu.jsx
+++ frontend/src/components/navbar/Menu.jsx
@@ -5,11 +5,12 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import { Link, useLocation } from 'react-router';
 
 export default function Menu() {
   const [open, setOpen] = React.useState(true);
@@ -18,7 +19,12 @@ export default function Menu() {
     setOpen(!open);
   };
 
+  const location = useLocation();
+  const path = location.pathname;
+  console.log(path);
+
   return (
+    <>
     <List
       sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
       component="nav"
@@ -30,23 +36,62 @@ export default function Menu() {
       }
     >
      
-      <ListItemButton onClick={handleClick}>
+      <ListItemButton onClick={handleClick} component={Link} to="/" selected={path === '/'}>
         <ListItemIcon>
             <DashboardIcon />
         </ListItemIcon>
         <ListItemText primary="All Clubs" />
         {open ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
+      
       <Collapse in={open} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
             <ListItemIcon>
-              <StarBorder />
+              <DashboardCustomizeIcon />
             </ListItemIcon>
             <ListItemText primary="Netherlands" />
+          </ListItemButton>
+
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemIcon>
+              <DashboardCustomizeIcon />
+            </ListItemIcon>
+            <ListItemText primary="India" />
+          </ListItemButton>
+
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemIcon>
+              <DashboardCustomizeIcon />
+            </ListItemIcon>
+            <ListItemText primary="France" />
           </ListItemButton>
         </List>
       </Collapse>
     </List>
+
+
+ <List
+      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader">
+          Creating Records
+        </ListSubheader>
+      }
+    >
+     
+      <ListItemButton component={Link} to="/create" selected={path === '/create'}>
+        <ListItemIcon>
+            <AddBoxIcon />
+        </ListItemIcon>
+        <ListItemText primary="Create Clubs" />
+  
+      </ListItemButton>
+      
+    </List>
+
+    </>
   );
 }

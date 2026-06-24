d81531d93185c54b03376312430c590169c98ac2
Moses Agunbiade <agunbiademoses2009@gmail.com>
2026-06-24 18:47:58 +0100

    added Axios components

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import { React, useState, useEffect} from 'react';
+import AxiosInstance from './Axios';
 
 const Create = () => {
+
+  const [country, setCountry] = useState([]);
+   const [league, setLeague] = useState([]);
+    const [characteristic, setCharacteristic] = useState([]);
+
+
+  console.log('country', country);
+  console.log('league', league);
+  console.log('characteristic', characteristic);
+
+  const GetData = () => {
+    AxiosInstance.get('country/').then((response) => {
+      setCountry(response.data);
+    });
+
+    AxiosInstance.get('league/').then((response) => {
+      setLeague(response.data);
+    });
+
+    AxiosInstance.get('characteristic/').then((response) => {
+      setCharacteristic(response.data);
+    });
+  };
+
+  useEffect(() => {
+    GetData();
+  }, []);
+
   return (
     <div>
       <h1>This is the Create Page</h1>
